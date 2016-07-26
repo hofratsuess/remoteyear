f1417ec987b5c8f075db3a9dd2714bce0c25eb84
rafaeladame <rafael@hofratsuess.ch>
2016-07-26 11:22:19 +0200

    Add and define fonts for City Name and City Details, design "ul" (spacing between .find("li")), background color for container (div)

--- app.js
+++ app.js
@@ -26,14 +26,33 @@ jQuery(document).ready(function($) {
   var renderSnippet = function(cityData) {
     // Create the container
     var main = $('<div></div>')
-                .css('font-family', 'sans-serif');
+                .css('font-family', 'Biryani', 'sans-serif')
+                .css('-webkit-font-smoothing', 'antialiased')
+                .css('background-color', '#5F32D6')
+                .css('padding', '15px')
+                .css('margin', '0px');
 
     // Add the city name
-    main.append($('<h1></h1>').text(cityData.name));
+    main.append($('<h1></h1>').text(cityData.name)
+                              .css('font-weight', '300')
+                              .css('font-size', '24px')
+                              .css('color', 'white')
+                              .css('text-decoration', 'underline')
+                              .css('margin', '0px')
+                            );
 
     // Add the details
     if (isAttributeFilled(cityData.details)) {
-      main.append($(cityData.details));
+      var cityDetails = $(cityData.details).css('list-style-type', 'disc')
+                                      .css('margin-top', '30px')
+                                      .css('padding-left', '15px');
+
+      cityDetails.find("li").css('padding-bottom', '8px')
+                  .css('font-weight', '300')
+                  .css('font-size', '14px')
+                  .css('line-height', '24px')
+                  .css('color', 'white');
+      main.append(cityDetails);
     }
 
     // Add the place of residence
@@ -47,5 +66,21 @@ jQuery(document).ready(function($) {
   console.log(getCityId($('.remoteyear').first()));
   loadCityData(getCityId($('.remoteyear').first()), function (d) { console.log(d); });
   loadCityData(getCityId($('.remoteyear').first()), renderSnippet);
-  loadCityData(3, function (d) { console.log(d); });
+  // loadCityData(3, function (d) { console.log(d); });
+
+  // var foo = function (arg1, arg2, etc, callback) {
+  //   var result = arg1 + arg2 + etc;
+  //   // return result;
+  //   callback(null, result);
+  // }
+  //
+  // var fooCallback = function (error, result) {
+  //   // if (error) {
+  //   //   throw error;
+  //   // }
+  //   console.log(result);
+  // }
+  //
+  // foo(1, 2, 3, fooCallback);
+
 });
