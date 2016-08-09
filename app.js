0c67ba4797671e0a35af875ceb44538e8f375e63
Satuk Kolan <satuk@hofratsuess.ch>
2016-08-09 15:19:03 +0200

    fixed the api url

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
       var weather_icon_container = $('<div></div>');
       main.append(weather_icon_container);
 
-      $.getJSON('https://project-2591251083691024669.appspot.com/' + lon + '/' + lat + '/', function(data) {
+      $.getJSON('https://project-2591251083691024669.appspot.com/locations/' + lon + '/' + lat + '/', function(data) {
         console.log(data);
 
         var weather_icon = $('<div></div>')
